af1922ca6df660fc2e1da60b6e2b921d4553cd4c
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-06-10 10:59:20 +0800

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 				<div className="mask">
 					<div className="d-flex justify-content-center align-items-center h-100">
 						<div className="text-white">
-							<h1 className="mb-3"><img src="./src/assets/images/brand/pokemonBrandName2.png" width="40%" /></h1>
+							<h1 className="mb-3"><img src="/src/assets/images/brand/pokemonBrandName2.png" width="40%" /></h1>
 							<form className="d-flex input-group w-auto mt-5 container" action="/pokedex" method="get">
 								<input id="pokemonName" name="pokemonName" type="search" className="form-control rounded" placeholder="Pokemon Search" aria-label="Search" aria-describedby="search-addon" required/>
 								<button className="btn bg-dark" type="submit">
