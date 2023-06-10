89e46cfd6e035fe09ff3c1c459f9deaebaccbce9
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-06-10 10:26:29 +0800

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
 	<header>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
         <button
           className="navbar-toggler"
@@ -20,7 +20,7 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <a className="navbar-brand mt-2 mt-lg-0" href="/">
             <img
-              src="./src/assets/images/brand/pokemonBrandName.png"
+              src="src/assets/images/brand/pokemonBrandName.png"
               height="30"
               alt="Pokemon"
               loading="lazy"
@@ -28,13 +28,13 @@ const Header = () => {
           </a>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to='/' className='nav-link'>Home</Link>
+              <Link to='/' className='nav-link hvr-underline-from-center'>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to='/' className='nav-link'>Pokedex</Link>
+              <Link to='/' className='nav-link hvr-underline-from-center'>Pokedex</Link>
             </li>
             <li className="nav-item">
-              <Link to='/' className='nav-link'>Pokecard</Link>
+              <Link to='/' className='nav-link hvr-underline-from-center'>Pokecard</Link>
             </li>
           </ul>
         </div>
